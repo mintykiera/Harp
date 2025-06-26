938496f097e5d964b41c636ba84cca2044672bac
Kiera <baileygraham2511@gmail.com>
2025-06-27 01:39:35 +0800

    last changes

--- commands/games/chess.js
+++ commands/games/chess.js
@@ -14,7 +14,8 @@ const os = require('os');
 
 const activePlayers = new Set();
 const activeGames = new Map();
-const stockfishFile = os.platform() === 'win32' ? 'stockfish.exe' : 'stockfish';
+const stockfishFile =
+  os.platform() === 'win32' ? 'stockfish.exe' : 'stockfish_bin';
 const stockfishPath = path.join(__dirname, '../../', stockfishFile);
 const difficultyLevels = {
   rookie: 1,
